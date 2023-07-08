ade506e3977002f7b340f5568d769b73201eb86b
Alessandro Serpi <alessandro.serpi@me.com>
2023-07-08 17:04:48 +0200

    Pass Splunk Cloud checks

--- additional_packaging.py
+++ additional_packaging.py
@@ -1,4 +1,6 @@
 import shutil
+import subprocess
+import sys
 from pathlib import Path
 
 
@@ -6,3 +8,15 @@ def additional_packaging(ta_name):
     output_path = Path("output") / ta_name
     shutil.copy(Path("LICENSE.md"), output_path)
     shutil.copy(Path("README.md"), output_path)
+
+    # Avoid binaries without source code (Splunk Cloud check)
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "charset-normalizer",
+                           "--no-binary=:all:", f"--target={output_path / 'lib'}", "--upgrade"])
+    shutil.rmtree(output_path / "lib/bin", ignore_errors=True)
+
+    # Patch botocore to avoid UDP connections (Splunk Cloud check)
+    with open(output_path / "lib/botocore/session.py", "r") as file:
+        filedata = file.read()
+    filedata = filedata.replace("SOCK_DGRAM", "SOCK_STREAM")  # Force TCP connections
+    with open(output_path / "lib/botocore/session.py", "w") as file:
+        file.write(filedata)
