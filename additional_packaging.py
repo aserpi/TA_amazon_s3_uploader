623a8114c1c4c66ae6b79c1e79591479a325b792
Alessandro Serpi <alessandro.serpi@me.com>
2026-03-14 20:40:00 +0100

    build: package changelog

--- additional_packaging.py
+++ additional_packaging.py
@@ -6,16 +6,27 @@ from pathlib import Path
 
 def additional_packaging(ta_name):
     output_path = Path("output") / ta_name
+    shutil.copy(Path("CHANGELOG.md"), output_path)
     shutil.copy(Path("LICENSE.md"), output_path)
     shutil.copy(Path("README.md"), output_path)
 
     # Avoid binaries without source code (Splunk Cloud check)
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "charset-normalizer",
-                           "--no-binary=:all:", f"--target={output_path / 'lib'}", "--upgrade"])
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "charset-normalizer",
+            "--no-binary=:all:",
+            f"--target={output_path / 'lib'}",
+            "--upgrade",
+        ]
+    )
     shutil.rmtree(output_path / "lib/bin", ignore_errors=True)
 
     # Patch botocore to avoid UDP connections (Splunk Cloud check)
-    with open(output_path / "lib/botocore/session.py", "r") as file:
+    with open(output_path / "lib/botocore/session.py") as file:
         filedata = file.read()
     filedata = filedata.replace("SOCK_DGRAM", "SOCK_STREAM")  # Force TCP connections
     with open(output_path / "lib/botocore/session.py", "w") as file:
