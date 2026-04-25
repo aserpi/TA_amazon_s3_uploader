f2eb574caebbbb0edab5749144f7ac91350ae7b6
Alessandro Serpi <alessandro.serpi@me.com>
2026-04-25 18:27:45 +0200

    ci: move to arm runner

--- additional_packaging.py
+++ additional_packaging.py
@@ -1,6 +1,4 @@
 import shutil
-import subprocess
-import sys
 from pathlib import Path
 
 
@@ -9,21 +7,6 @@ def additional_packaging(ta_name):
     shutil.copy(Path("CHANGELOG.md"), output_path)
     shutil.copy(Path("LICENSE.md"), output_path)
     shutil.copy(Path("README.md"), output_path)
-
-    # Avoid binaries without source code (Splunk Cloud check)
-    subprocess.check_call(
-        [
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            "charset-normalizer",
-            "--no-binary=:all:",
-            f"--target={output_path / 'lib'}",
-            "--upgrade",
-        ]
-    )
-    shutil.rmtree(output_path / "lib/bin", ignore_errors=True)
 
     # Patch botocore to avoid UDP connections (Splunk Cloud check)
     with open(output_path / "lib/botocore/session.py") as file:
